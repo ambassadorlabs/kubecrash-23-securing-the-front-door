a6d11882c2e43116fbd9d4be82c8bddfd48455c1
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2023-03-13 12:00:51 -0600

    create cert-manager cluster issuers

--- iac/certmanager/index.ts
+++ iac/certmanager/index.ts
@@ -1,5 +1,7 @@
 import * as k8s from '@pulumi/kubernetes'
+import * as certmanager from '../crds/certmanager'
 import * as cluster from '../cluster'
+import config from '../config'
 
 const namespace = new k8s.core.v1.Namespace('cert-manager', {
   metadata: {
@@ -20,3 +22,69 @@ export const release = new k8s.helm.v3.Release('cert-manager', {
     repo: 'https://charts.jetstack.io'
   },
 }, { provider: cluster.provider, dependsOn: crds })
+
+const cloudflareSecret = new k8s.core.v1.Secret('cert-manager-cloudflare', {
+  metadata: {
+    name: 'cloudflare-api-token-secret',
+    namespace: namespace.metadata.name,
+  },
+  stringData: {
+    'api-token': config.requireSecret('cloudflareApiToken'),
+  }
+}, { provider: cluster.provider })
+
+export const stagingClusterIssuer = new certmanager.certmanager.v1.ClusterIssuer('staging-cluster-issuer', {
+  metadata: {
+    name: 'letsencrypt-staging',
+    namespace: namespace.metadata.name
+  },
+  spec: {
+    acme: {
+      email: config.requireSecret('acmeEmail'),
+      server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+      privateKeySecretRef: {
+        name: 'staging-issuer-account-key'
+      },
+      solvers: [
+        {
+          dns01: {
+            cloudflare: {
+              apiTokenSecretRef: {
+                name: cloudflareSecret.metadata.name,
+                key: 'apiKey'
+              }
+            }
+          }
+        }
+      ]
+    },
+  }
+}, {provider: cluster.provider, dependsOn: release })
+
+ export const productionClusterIssuer = new certmanager.certmanager.v1.ClusterIssuer('production-cluster-issuer', {
+   metadata: {
+     name: 'letsencrypt-production',
+     namespace: namespace.metadata.name
+   },
+   spec: {
+     acme: {
+       email: config.requireSecret('acmeEmail'),
+       server: 'https://acme-v02.api.letsencrypt.org/directory',
+       privateKeySecretRef: {
+         name: 'production-issuer-account-key'
+       },
+       solvers: [
+         {
+           dns01: {
+             cloudflare: {
+               apiTokenSecretRef: {
+                 name: cloudflareSecret.metadata.name,
+                 key: 'apiKey'
+               }
+             }
+           }
+         }
+       ]
+     }
+   }
+ }, {provider: cluster.provider, dependsOn: release })
